63d5b001080bf3dc9bc561bb4bcb72748187d052
Maverick Studer <maverick.studer95@gmail.com>
2020-11-30 14:56:22 +0100

    document and importer implementation

---
+++ src/main/java/Document.java
@@ -0,0 +1,55 @@
+public class Document {
+
+    // Document identifier
+    private Long id;
+
+    // Document title
+    private String title;
+
+    // Document URL
+    private String url;
+
+    // Document content
+    private String[] content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String[] getContent() {
+        return content;
+    }
+
+    public void setContent(String[] content) {
+        this.content = content;
+    }
+
+    public Document(Long id) {
+        this.id = id;
+    }
+
+    public Document(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
---
+++ src/main/java/Importer.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.apache.commons.io.FileUtils.listFiles;
+
+public class Importer {
+
+    private void importFile(File file){
+        if(file == null || !file.isDirectory()) {
+            System.out.println("No Directory provided!");
+            return;
+        }
+
+        String[] extensions = new String[] {"xml", "XML"};
+        List<File> files = listFiles(file, extensions, true).stream().collect(Collectors.toList());
+
+        for(File f : files) {
+            String name = f.getName();
+            Long size = f.length();
+            System.out.println(String.format("Name: %s Size: %d Bytes", name, size));
+        }
+
+    }
+}

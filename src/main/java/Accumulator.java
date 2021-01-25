2794d766babae08e43b560e6a80579627ed1fef5
Maverick Studer <maverick.studer95@gmail.com>
2021-01-25 16:02:52 +0100

    InvertedIndex

---
+++ src/main/java/Accumulator.java
@@ -0,0 +1,21 @@
+public class Accumulator {
+    private long did;
+
+    private double score;
+
+    public long getDid() {
+        return did;
+    }
+
+    public void setDid(long did) {
+        this.did = did;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
--- src/main/java/Importer.java
+++ src/main/java/Importer.java
@@ -10,7 +10,6 @@ import static org.apache.commons.io.FileUtils.listFiles;
 public class Importer {
 
     private Parser parser = new Parser();
-    private ImportDataBaseHelper importDataBaseHelper = new ImportDataBaseHelper();
 
     public void importFile(File file){
         if(file == null || !file.isDirectory()) {
@@ -21,13 +20,13 @@ public class Importer {
         String[] extensions = new String[] {"xml", "XML"};
         List<File> files = listFiles(file, extensions, true).stream().collect(Collectors.toList());
 
+        ImportDataBaseHelper importDataBaseHelper = new ImportDataBaseHelper();
         for(File f : files) {
             String name = f.getName();
             Long size = f.length();
             System.out.println(String.format("Name: %s Size: %d Bytes", name, size));
             Document document = parser.parse(f);
             importDataBaseHelper.saveDocument(document);
-
         }
     }
 }

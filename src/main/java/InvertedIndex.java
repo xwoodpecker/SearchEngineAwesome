f134ce43b995273de9561adccac204585e90a814
Maverick Studer <maverick.studer95@gmail.com>
2021-01-25 15:03:17 +0100

    InvertedIndex

---
+++ src/main/java/InvertedIndex.java
@@ -0,0 +1,19 @@
+public class InvertedIndex {
+    
+    public List<Posting> getIndexList(String term) {
+
+    }
+
+    public int getDF(String term){
+
+    }
+
+    public int getSize() {
+
+    }
+
+    public int getLength(long did) {
+
+    }
+
+}
